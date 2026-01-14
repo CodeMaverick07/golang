2ab872f7f84767487f43988102fa33777b136c5f
CodeMaverick07 <hemantjatal59@gmail.com>
2026-01-15 01:50:48 +0530

    feat: implement websocket server with client management and event handling

---
+++ websocket/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	rootCtx := context.Background()
+	ctx, cancel := context.WithCancel(rootCtx)
+	defer cancel()
+	setupAPI(ctx)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setupAPI(ctx context.Context) {
+
+	manager := NewManager(ctx)
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.serveWS)
+	http.HandleFunc("/login", manager.loginHandler)
+	http.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, len(manager.clients))
+	})
+}
---
+++ websocket/manager.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	websocketUpgrader = websocket.Upgrader{
+		CheckOrigin:     checkOrigin,
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+	handlers map[string]EventHandler
+	otps     RetentionMap
+}
+
+func NewManager(ctx context.Context) *Manager {
+	m := &Manager{
+		clients:  make(ClientList),
+		handlers: make(map[string]EventHandler),
+		otps:     NewRetentionMap(ctx, 5*time.Second),
+	}
+	m.setupEventHandlers()
+	return m
+}
+
+func (m *Manager) setupEventHandlers() {
+	m.handlers[EventSendMessage] = SendMessageHandler
+	m.handlers[EventChangeRoom] = ChatRoomHandler
+}
+func SendMessageHandler(event Event, c *Client) error {
+	// Marshal Payload into wanted format
+	var chatevent SendMessageEvent
+	if err := json.Unmarshal(event.Payload, &chatevent); err != nil {
+		return fmt.Errorf("bad payload in request: %v", err)
+	}
+
+	// Prepare an Outgoing Message to others
+	var broadMessage NewMessageEvent
+
+	broadMessage.Sent = time.Now()
+	broadMessage.Message = chatevent.Message
+	broadMessage.From = chatevent.From
+
+	data, err := json.Marshal(broadMessage)
+	if err != nil {
+		return fmt.Errorf("failed to marshal broadcast message: %v", err)
+	}
+
+	// Place payload into an Event
+	var outgoingEvent Event
+	outgoingEvent.Payload = data
+	outgoingEvent.Type = EventNewMessage
+	// Broadcast to all other Clients
+	for client := range c.manager.clients {
+		// Only send to clients inside the same chatroom
+		if client.chatroom == c.chatroom {
+			client.egress <- outgoingEvent
+		}
+
+	}
+	return nil
+}
+
+type ChangeRoomEvent struct {
+	Name string `json:"name"`
+}
+
+// ChatRoomHandler will handle switching of chatrooms between clients
+func ChatRoomHandler(event Event, c *Client) error {
+	// Marshal Payload into wanted format
+	var changeRoomEvent ChangeRoomEvent
+	if err := json.Unmarshal(event.Payload, &changeRoomEvent); err != nil {
+		return fmt.Errorf("bad payload in request: %v", err)
+	}
+
+	// Add Client to chat room
+	c.chatroom = changeRoomEvent.Name
+
+	return nil
+}
+
+func (m *Manager) routeEvent(event Event, c *Client) error {
+	if handler, ok := m.handlers[event.Type]; ok {
+		if err := handler(event, c); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return errors.New("there is no such event type")
+	}
+}
+
+func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
+
+	// Grab the OTP in the Get param
+	otp := r.URL.Query().Get("otp")
+	if otp == "" {
+		// Tell the user its not authorized
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Verify OTP is existing
+	if !m.otps.VerifyOTP(otp) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	log.Println("New connection")
+	// Begin by upgrading the HTTP request
+	conn, err := websocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Create New Client
+	client := NewClient(conn, m)
+	// Add the newly created client to the manager
+	m.addClient(client)
+
+	go client.readMessages()
+	go client.writeMessages()
+}
+
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+	m.clients[client] = true
+}
+
+func (m *Manager) removeClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	_, ok := m.clients[client]
+	if ok {
+		client.connection.Close()
+		delete(m.clients, client)
+	}
+}
+
+func checkOrigin(r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+	switch origin {
+	case "http://localhost:8080":
+		return true
+
+	default:
+		return false
+	}
+}
+
+func (m *Manager) loginHandler(w http.ResponseWriter, r *http.Request) {
+
+	type userLoginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	var req userLoginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Authenticate user / Verify Access token, what ever auth method you use
+	if req.Username == "percy" && req.Password == "123" {
+		// format to return otp in to the frontend
+		type response struct {
+			OTP string `json:"otp"`
+		}
+
+		// add a new OTP
+		otp := m.otps.NewOTP()
+
+		resp := response{
+			OTP: otp.Key,
+		}
+
+		data, err := json.Marshal(resp)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// Return a response to the Authenticated user with the OTP
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+		return
+	}
+
+	// Failure to auth
+	w.WriteHeader(http.StatusUnauthorized)
+}
